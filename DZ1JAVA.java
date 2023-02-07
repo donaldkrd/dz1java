92f30159fff1c6c6968caada8df02a032cb1afd4
Alexey Utkin <alex_habar@mail.ru>
2023-02-07 18:53:21 +0300

    dz1

--- DZ1JAVA.java
+++ DZ1JAVA.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class DZ1JAVA {
     public static void main(String[] args) {
         task1();
     }    
 static void task1(){
-    
-}
+    //Вычислить n-ое треугольного число(сумма чисел от 1 до n)
+    Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
 
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+}
 
 }
 
